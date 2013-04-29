b36af36a362bdff0bc2da401f51fc924815101c8
Friedrich Lindenberg <friedrich@pudo.org>
2013-04-29 23:32:44 +0200

    Simple status monitor.

---
+++ static/js/status.js
@@ -0,0 +1,24 @@
+$(function() {
+  var $areaLatest = $('#area-latest'),
+      $areaStats = $('#area-stats'),
+      tmplLatest = Handlebars.compile($('#tmpl-latest').html()),
+      tmplStats = Handlebars.compile($('#tmpl-stats').html());
+
+  function updatePage() {
+    $.getJSON('/stats', function(data) {
+      $.each(data.queries, function(i, q) {
+        if (q.type === 'track') data.queries[i].is_track = true;
+        if (q.type === 'follow') data.queries[i].is_follow = true;
+      });
+      $areaStats.html(tmplStats(data));
+    });
+    $.getJSON('/latest', function(data) {
+      $areaLatest.html(tmplLatest({statuses: data}));
+    });
+  }
+
+  setInterval(updatePage, 2000);
+});
+
+
+
