01fec8dfbaa172c88039fd5e8c8d923cf329716e
Friedrich Lindenberg <friedrich@pudo.org>
2013-05-30 18:55:36 +0200

    Update deployment to work against EC2 host.

--- fabfile.py
+++ fabfile.py
@@ -1,35 +1,37 @@
 
 from fabric.api import *
 
-env.hosts = ['norton.pudo.org']
-deploy_dir = '/var/www/twindle.pudo.org/'
-backup_dir = '/var/www/opendatalabs.org/backup'
-remote_user = 'fl'
+env.hosts = ['twindle.pudo.org']
+env.key_filename = '/Users/lindenbergf/ec2/spon_ec2.pem'
+env.user = 'ubuntu'
+deploy_dir = '/home/ubuntu/twindle/'
+backup_dir = '/home/ubuntu/backup'
+
 
 def deploy():
     run('mkdir -p ' + backup_dir)
-    run('pg_dump -f ' + backup_dir + '/spon_twitter-`date +%Y%m%d`.sql spon_twitter')
+    run('pg_dump -f ' + backup_dir + '/twindle-`date +%Y%m%d`.sql twindle')
     with cd(deploy_dir + 'app'):
         run('git pull')
         run('git reset --hard HEAD')
         run('npm install .')
     sudo('supervisorctl reread')
-    sudo('supervisorctl restart twindle.pudo.org')
+    sudo('supervisorctl reload')
+
 
 def install():
     sudo('rm -rf ' + deploy_dir)
     sudo('mkdir -p ' + deploy_dir)
-    sudo('chown -R ' + remote_user + ' ' + deploy_dir)
+    sudo('chown -R ' + env.user + ' ' + deploy_dir)
     put('deploy/*', deploy_dir)
 
-    sudo('mv ' + deploy_dir + 'nginx.conf /etc/nginx/sites-available/twindle.pudo.org')
-    sudo('ln -sf /etc/nginx/sites-available/twindle.pudo.org /etc/nginx/sites-enabled/twindle.pudo.org')
+    sudo('mv ' + deploy_dir + 'nginx.conf /etc/nginx/sites-available/twindle')
+    sudo('ln -sf /etc/nginx/sites-available/twindle /etc/nginx/sites-enabled/twindle')
     sudo('service nginx restart')
 
-    sudo('ln -sf ' + deploy_dir + 'supervisor.conf /etc/supervisor/conf.d/twindle.pudo.org.conf')
+    sudo('ln -sf ' + deploy_dir + 'supervisor.conf /etc/supervisor/conf.d/twindle.conf')
     run('mkdir ' + deploy_dir + 'logs')
-    sudo('chown -R www-data.www-data ' + deploy_dir + 'logs')
+    #sudo('chown -R ubtuntu.ubtuntu ' + deploy_dir + 'logs')
 
     run('git clone git://github.com/pudo/twindle.git ' + deploy_dir + 'app')
     deploy()
-
