5e03cc909c659ff8b6bcc5d8a55424dd1892f8f7
Friedrich Lindenberg <friedrich@pudo.org>
2013-06-14 10:32:46 +0200

    Fix up the script.

--- analysis/dumpraw.py
+++ analysis/dumpraw.py
@@ -3,23 +3,24 @@ import json
 from common import engine
 
 log = logging.getLogger(__name__)
-table = engine.get_table('raw')
+raw_table = engine.get_table('raw')
+#status_table = engine.get_table('raw_dumped')
 
-BATCH_SIZE = 5000
+BATCH_SIZE = 10000
 
 
 def dump_batches():
-    if len(table) < BATCH_SIZE:
+    if len(raw_table) < BATCH_SIZE:
         log.info("Not enough entries remaining.")
         return False
-    data, min_id = [], None
+    data, min_id, max_id = [], None, 0
     log.info("Fetching %s raw tweets...", BATCH_SIZE)
     engine.begin()
-    for row in table.find(_limit=BATCH_SIZE, order_by=['id']):
+    for row in list(raw_table.find(_limit=BATCH_SIZE, order_by=['id'])):
         if min_id is None:
             min_id = row['id']
         data.append(json.loads(row['json']))
-        table.delete(id=row['id'])
+        raw_table.delete(id=row['id'])
     log.info("Saving file...")
     fh = open('dumps/raw_%s.json' % min_id, 'wb')
     json.dump(data, fh)
