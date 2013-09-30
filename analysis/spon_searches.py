de1be8fe056e8adc7f41104d5a64d51afab22dac
Friedrich Lindenberg <friedrich@pudo.org>
2013-09-30 04:49:23 +0000

    ignore searches marked as inactive

--- analysis/spon_searches.py
+++ analysis/spon_searches.py
@@ -2,12 +2,14 @@ import re
 from pprint import pprint
 from lxml import etree
 from common import normalize
-from classifydb import classify_tweets
+from classifydb import classify_tweets, delete_old_tags
 
 def load_searches():
     doc = etree.parse('http://www.spiegel.de/xml/xml-61163.xml')
     searches = {}
     for search_el in doc.findall('.//search'):
+        if 'inaktiv' in search_el.get('type', ''):
+            continue
         terms = search_el.findall('.//term')
         #terms = sorted([normalize(t.text) for t in terms])
         terms = sorted([t.text.lower().strip() for t in terms])
@@ -25,5 +27,6 @@ def load_searches():
 
 if __name__ == '__main__':
     rules = load_searches()
-    classify_tweets(rules)
+    delete_old_tags(rules)
+    #classify_tweets(rules)
 
